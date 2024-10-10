d2d731cd6a3c5ee3dc4fd7b0eb96337a6679ea03
Andejecruher <andejecruher@gmail.com>
2024-10-10 16:34:30 -0600

    Actualizar importaciones y configuración de enrutamiento

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,24 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Home from "./Pages/Home/Home.jsx";
 import { ThemeProvider } from "@mui/material/styles";
-import CssBaseline from "@mui/material/CssBaseline";
-import theme from "./components/Themes/theme";
-// Import styles of packages that you've installed.
-// All packages except `@mantine/hooks` require styles imports
-import '@mantine/core/styles.css';
-
+import { RouterProvider } from "react-router-dom";
 import { MantineProvider } from '@mantine/core';
+import CssBaseline from "@mui/material/CssBaseline";
+import theme from "@src/components/Themes/theme";
+import router from "@src/router/router";
+
+import '@mantine/core/styles.css';
 import "./index.css";
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-  <MantineProvider>
-    <ThemeProvider theme={theme}>
-      <CssBaseline />{" "}
-      {/* Esto asegura que los estilos básicos sean reseteados */}
-      <Home />
-    </ThemeProvider>
-  </MantineProvider>
+    <MantineProvider>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />{" "}
+        {/* Esto asegura que los estilos básicos sean reseteados */}
+        <RouterProvider router={router} />
+      </ThemeProvider>
+    </MantineProvider>
   </React.StrictMode>
 );
