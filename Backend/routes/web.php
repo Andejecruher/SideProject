083b750ae20f951295e0be0c953563a9d204e918
Andejecruher <andejecruher@gmail.com>
2024-10-14 16:12:26 -0600

    Actualizar configuración de archivos ignorados y rutas en el repositorio

--- Backend/routes/web.php
+++ Backend/routes/web.php
@@ -1,29 +1,15 @@
 <?php
 
-use App\Http\Livewire\BootstrapTables;
-use App\Http\Livewire\Components\Buttons;
-use App\Http\Livewire\Components\Forms;
-use App\Http\Livewire\Components\Modals;
-use App\Http\Livewire\Components\Notifications;
-use App\Http\Livewire\Components\Typography;
 use App\Http\Livewire\Dashboard;
 use App\Http\Livewire\Err404;
 use App\Http\Livewire\Err500;
 use App\Http\Livewire\ResetPassword;
 use App\Http\Livewire\ForgotPassword;
-use App\Http\Livewire\Lock;
 use App\Http\Livewire\Auth\Login;
 use App\Http\Livewire\Profile;
 use App\Http\Livewire\Auth\Register;
-use App\Http\Livewire\ForgotPasswordExample;
-use App\Http\Livewire\Index;
-use App\Http\Livewire\LoginExample;
 use App\Http\Livewire\ProfileExample;
-use App\Http\Livewire\RegisterExample;
-use App\Http\Livewire\Transactions;
 use Illuminate\Support\Facades\Route;
-use App\Http\Livewire\ResetPasswordExample;
-use App\Http\Livewire\UpgradeToPro;
 use App\Http\Livewire\Users\Users;
 
 /*
@@ -55,36 +41,4 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile-example', ProfileExample::class)->name('profile-example');
     Route::get('/users', Users::class)->name('users');
     Route::get('/dashboard', Dashboard::class)->name('dashboard');
-
-    Route::group(['prefix' => 'articulos'], function () {
-        Route::get('/', [ArticuloController::class, 'index']);
-        Route::get('/{id}', [ArticuloController::class, 'show']);
-        Route::post('/', [ArticuloController::class, 'store']);
-        Route::put('/{id}', [ArticuloController::class, 'update']);
-        Route::delete('/{id}', [ArticuloController::class, 'destroy']);
-    });
-
-    Route::group(['prefix' => 'tags'], function () {
-        Route::get('/', [TagController::class, 'index']);
-        Route::get('/{id}', [TagController::class, 'show']);
-        Route::post('/', [TagController::class, 'store']);
-        Route::put('/{id}', [TagController::class, 'update']);
-        Route::delete('/{id}', [TagController::class, 'destroy']);
-    });
-
-    Route::group(['prefix' => 'comentarios'], function () {
-        Route::get('/', [ComentarioController::class, 'index']);
-        Route::get('/{id}', [ComentarioController::class, 'show']);
-        Route::post('/', [ComentarioController::class, 'store']);
-        Route::put('/{id}', [ComentarioController::class, 'update']);
-        Route::delete('/{id}', [ComentarioController::class, 'destroy']);
-    });
-
-    Route::group(['prefix' => 'categorias'], function () {
-        Route::get('/', [CategoriaController::class, 'index']);
-        Route::get('/{id}', [CategoriaController::class, 'show']);
-        Route::post('/', [CategoriaController::class, 'store']);
-        Route::put('/{id}', [CategoriaController::class, 'update']);
-        Route::delete('/{id}', [CategoriaController::class, 'destroy']);
-    });
 });
