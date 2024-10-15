3a62b002b17a6ee7dcc85f3d6ce1b4b7cdc3fbfd
Andejecruher <andejecruher@gmail.com>
2024-10-14 20:01:12 -0600

    Actualizar configuración de archivos ignorados y rutas en el repositorio

--- Backend/resources/views/layouts/base.blade.php
+++ Backend/resources/views/layouts/base.blade.php
@@ -123,12 +123,6 @@
 
 <body>
 <title>Andejecruher</title>
-    @if(env('IS_DEMO'))
-        <!-- Google Tag Manager (noscript) -->
-        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-THQTXJ7" height="0" width="0"
-            style="display:none;visibility:hidden"></iframe></noscript>
-        <!-- End Google Tag Manager (noscript) -->
-    @endif
 
     {{ $slot }}
 
